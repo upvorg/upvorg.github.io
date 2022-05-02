5e7c4f9e74083e44e2ee1828fa5120093cff8608
shiyiya <shiyiya.11@gmail.com>
2022-05-02 06:47:22 +0000

    .

---
+++ types/web-env.d.ts
@@ -0,0 +1,12 @@
+declare module '*.css'
+declare module '*.less'
+declare module '*.png'
+declare module '*.svg' {
+  const url: string
+  export default url
+}
+
+declare var __DEV__: boolean
+declare var __API_HOST__: string
+declare var __STORAGE_HOST__: string
+declare var __ADMIN_HOST__: string
