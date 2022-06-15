48eef1c6d7c4d045d14555dc982ce2522ccbdb7c
CY <shiyiya.11@gmail.com>
2022-06-15 06:12:58 +0000

    .

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # web
 
 [![Server Deploy](https://github.com/upvorg/web2/actions/workflows/deploy.yml/badge.svg)](https://github.com/upvorg/web2/actions/workflows/deploy.yml)
-
-- https://skeletonreact.com/
--- packages/shared/f12.ts
+++ packages/shared/f12.ts
@@ -1,34 +1,13 @@
 if (!__DEV__ && new URLSearchParams(window.location.search).get('debug') == null) {
-  require('disable-devtool')({
-    url: 'https://baidu.com/',
-    disableMenu: false,
-    detectors: [0, 1, 2, 3, 4],
-    clearIntervalWhenDevOpenTrigger: true,
-    clearLog: false
-  })
-
-  // listen browser width
-  require('devtools-detect')
-  window.addEventListener('devtoolschange', (event) => {
-    //@ts-ignore
-    if (event.detail.open) {
-      window.location.href = 'https://www.baidu.com'
-      window.close()
-    }
-  })
-
-  // Error: Does not work after build.
-  // require('detect-devtools-via-debugger-heartstop')
-  // // @ts-ignore
-  // globalThis.devtoolsDetector.config.onDetectOpen = (e) => {
-  //   window.location.href = 'https://www.baidu.com'
-  //   window.close()
-  // }
-
-  //feedback
-  setInterval(() => {
-    debugger
+  import('devtools-detector').then(function name(devtoolsDetector) {
+    devtoolsDetector.addListener(function (isOpen: boolean) {
+      if (isOpen) {
+        window.close()
+        window.location.href = 'https://www.baidu.com/s?wd=ip'
+      }
+    })
+    devtoolsDetector.launch()
   })
 }
 
-export {}
+export { }
