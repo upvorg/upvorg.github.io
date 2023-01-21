c7cb81d1c1c9ac725ce473c437e4b2d278b37ccb
CY <shiyiya.11@gmail.com>
2023-01-21 23:40:57 +0800

    Update index.tsx

--- packages/index/src/pages/about/index.tsx
+++ packages/index/src/pages/about/index.tsx
@@ -8,7 +8,8 @@ const content = `
 free animes no ads.
 
 [APP 下载](https://app.upv.life/) |
-[QQ 交流群](https://jq.qq.com/?_wv=1027&k=5lfSD1B)
+[QQ 交流群](https://jq.qq.com/?_wv=1027&k=5lfSD1B) |
+[![Discord](https://img.shields.io/discord/1017615537234264185.svg?label=&logo=discord&logoColor=fff&color=7389D8&labelColor=6A7EC2&style=flat-square)](https://discord.gg/hzjxYyPbKh)
 `
 
 const AboutPage: React.FC = () => {
