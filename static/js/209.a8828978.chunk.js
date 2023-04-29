c194fea1283d8b482af6962769e86e7768669e0c
shiyiya <shiyiya@users.noreply.github.com>
2023-04-29 12:33:34 +0000

    deploy: 66e50559ccbfc2b0fa3bc484b7c69fdcfb7db67d

---
+++ static/js/209.a8828978.chunk.js
@@ -0,0 +1,15 @@
+"use strict";(self.webpackChunk_web_index=self.webpackChunk_web_index||[]).push([[209],{8209:function(D,i,e){e.r(i);var o=e(5281),c=e(3661),m=e(6309);const v=`
+## About
+
+Free animes no ads.
+
+[Discord](https://discord.gg/Ue3p4CnRDP) |
+[GitHub](https://github.com/upvorg) |
+[APP Download](https://github.com/upvorg/cdn/releases) |
+[QQ Group](https://jq.qq.com/?_wv=1027&k=5lfSD1B)
+
+| ![](https://i.ibb.co/8PQ2P1Z/Screenshot-20230303-203520.jpg) | ![](https://i.ibb.co/yfcxbyZ/Screenshot-20230227-212716.jpg) | ![image](https://i.ibb.co/kKpLTs6/Screenshot-20230303-204742.jpg) |
+|---|---|-------------------------------------------------------------------|
+
+
+`,p=()=>(0,o.jsxs)("div",{className:"container",children:[(0,o.jsx)(m.q,{children:(0,o.jsx)("title",{children:"\u5173\u4E8E - UPV - free animes no ads"})}),(0,o.jsx)(c.U,{type:"render",value:v})]});i.default=p},3661:function(D,i,e){e.d(i,{U:function(){return B}});var o=e(5281),c=e(1479),m=e(3739),v=e(2086),p=e(6569),E=e(1975),C=e(7697),x=e(1748),h=e(1785),Z=e(4617),M=e(7003),K=e(2678),G={},A=Object.defineProperty,R=Object.defineProperties,S=Object.getOwnPropertyDescriptors,y=Object.getOwnPropertySymbols,U=Object.prototype.hasOwnProperty,I=Object.prototype.propertyIsEnumerable,g=(s,r,t)=>r in s?A(s,r,{enumerable:!0,configurable:!0,writable:!0,value:t}):s[r]=t,O=(s,r)=>{for(var t in r||(r={}))U.call(r,t)&&g(s,t,r[t]);if(y)for(var t of y(r))I.call(r,t)&&g(s,t,r[t]);return s},b=(s,r)=>R(s,S(r)),N=(s,r,t)=>new Promise((u,l)=>{var f=n=>{try{d(t.next(n))}catch(a){l(a)}},P=n=>{try{d(t.throw(n))}catch(a){l(a)}},d=n=>n.done?u(n.value):Promise.resolve(n.value).then(f,P);d((t=t.apply(s,r)).next())}),j,w;const L={render:{view:{menu:!1,md:!1,html:!0},classname:"markdown_render"},editor:{view:{menu:!0,md:!0,html:!1},classname:"markdown_editor"}},T=(0,c.l)().use(m.Z).use(E.Z).use(v.Z,{allowDangerousHtml:!0}).use(p.Z).use(C.Z,{properties:"attr"}).use(x.Z,b(O({},h.R),{attributes:b(O({},h.R.attributes),{img:[...((w=(j=h.R)==null?void 0:j.attributes)==null?void 0:w.img)||[],["style"]]})})).use(Z.Z),B=s=>{const{value:r="",type:t,onChange:u,customClassName:l="",placeholder:f=""}=s,{view:P,classname:d}=L[t];let n=`markdown ${d}`;return l&&(n+=` ${l}`),(0,o.jsx)(M.ZP,{className:n,readOnly:t==="render",view:P,value:r,htmlClass:"markdown-body",renderHTML:a=>N(void 0,null,function*(){return(yield T.process(a)).toString()}),onChange:a=>u==null?void 0:u(a.text),placeholder:f})}}}]);
--- static/js/467.1a15bc29.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_web_index=self.webpackChunk_web_index||[]).push([[467],{1467:function(l,s,n){n.r(s);var e=n(6345),t=n(7807);const d=()=>(0,e.jsxs)(e.Fragment,{children:[(0,e.jsx)(t.q,{children:(0,e.jsx)("title",{children:"404 - UPV - free animes no ads"})}),(0,e.jsx)("h1",{children:"404"}),(0,e.jsx)("p",{children:"Page not found"})]});s.default=d}}]);
