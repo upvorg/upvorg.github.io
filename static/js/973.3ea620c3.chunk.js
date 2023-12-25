3e8dfcf8968db8c68f2502000f4b72916e38ee14
shiyiya <shiyiya@users.noreply.github.com>
2023-12-25 02:26:40 +0000

    deploy: 3362dc1ec4f48e5044b7a57ec5d4b91f05db6107

---
+++ static/js/973.3ea620c3.chunk.js
@@ -0,0 +1,15 @@
+"use strict";(self.webpackChunk_web_index=self.webpackChunk_web_index||[]).push([[973],{2973:(D,d,e)=>{e.r(d),e.d(d,{default:()=>p});var o=e(8694),m=e(9463),u=e(4581);const v=`
+## About
+
+Free animes no ads.
+
+[Discord](https://discord.gg/Ue3p4CnRDP) |
+[GitHub](https://github.com/upvorg) |
+[APP Download](https://github.com/upvorg/cdn/releases) |
+[QQ Group](https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=OWf2sPbc2FX2Q0yWc6kePlYMhfNNfiIC&authKey=vs38F6pgitYXKOtHyWpNSk%2BWrMz%2BA5ZlpQCY00OqbfUtihtRDKiNmLOCeXQryUIY&noverify=0&group_code=757260467)
+
+| ![](https://i.ibb.co/8PQ2P1Z/Screenshot-20230303-203520.jpg) | ![](https://i.ibb.co/yfcxbyZ/Screenshot-20230227-212716.jpg) | ![image](https://i.ibb.co/kKpLTs6/Screenshot-20230303-204742.jpg) |
+|---|---|-------------------------------------------------------------------|
+
+
+`,p=()=>(0,o.jsxs)("div",{className:"container",children:[(0,o.jsx)(u.q,{children:(0,o.jsx)("title",{children:"\u5173\u4E8E - UPV - free animes no ads"})}),(0,o.jsx)(m.U,{type:"render",value:v})]})},9463:(D,d,e)=>{e.d(d,{U:()=>S});var o=e(8381),m=e(1382),u=e(7164),v=e(9121),y=e(7086),p=e(2388),j=e(5481),A=e(7901),h=e(4866),M=e(8250),Z=e(213),T=e(7318);const B={};var N=Object.defineProperty,U=Object.defineProperties,R=Object.getOwnPropertyDescriptors,g=Object.getOwnPropertySymbols,x=Object.prototype.hasOwnProperty,I=Object.prototype.propertyIsEnumerable,O=(s,r,t)=>r in s?N(s,r,{enumerable:!0,configurable:!0,writable:!0,value:t}):s[r]=t,b=(s,r)=>{for(var t in r||(r={}))x.call(r,t)&&O(s,t,r[t]);if(g)for(var t of g(r))I.call(r,t)&&O(s,t,r[t]);return s},E=(s,r)=>U(s,R(r)),K=(s,r,t)=>new Promise((c,l)=>{var P=n=>{try{i(t.next(n))}catch(a){l(a)}},f=n=>{try{i(t.throw(n))}catch(a){l(a)}},i=n=>n.done?c(n.value):Promise.resolve(n.value).then(P,f);i((t=t.apply(s,r)).next())}),C,w;const W={render:{view:{menu:!1,md:!1,html:!0},classname:"markdown_render"},editor:{view:{menu:!0,md:!0,html:!1},classname:"markdown_editor"}},L=(0,m.l)().use(u.Z).use(p.Z).use(v.Z,{allowDangerousHtml:!0}).use(y.Z).use(j.Z,{properties:"attr"}).use(A.Z,E(b({},h.R),{attributes:E(b({},h.R.attributes),{img:[...((w=(C=h.R)==null?void 0:C.attributes)==null?void 0:w.img)||[],["style"]]})})).use(M.Z),S=s=>{const{value:r="",type:t,onChange:c,customClassName:l="",placeholder:P=""}=s,{view:f,classname:i}=W[t];let n=`markdown ${i}`;return l&&(n+=` ${l}`),o.createElement(Z.ZP,{className:n,readOnly:t==="render",view:f,value:r,htmlClass:"markdown-body",renderHTML:a=>K(void 0,null,function*(){return(yield L.process(a)).toString()}),onChange:a=>c==null?void 0:c(a.text),placeholder:P})}}}]);
