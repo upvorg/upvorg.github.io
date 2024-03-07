a06f8f21248dabbd1cafe63f46e63172b29093ec
shiyiya <shiyiya@users.noreply.github.com>
2024-03-07 02:48:08 +0000

    deploy: a5cfa5f54002de3332e658aeacab9a459afe8e06

--- js/background.js
+++
@@ -1,1 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function(i){var e,a;var n=((a=(e=navigator==null?void 0:navigator.userAgentData)==null?void 0:e.platform)==null?void 0:a.toLocaleLowerCase())==="windows";chrome.app.window.create("/index.html",{id:"upv",innerBounds:{width:1024,height:700},minWidth:1024,minHeight:700,frame:n?{color:"#6668ab"}:"chrome"})});
