b2635ada5d6568c5ea96c8c4f1c371836cffce37
shiyiya <shiyiya.11@gmail.com>
2024-06-08 15:05:23 +0800

    -

--- packages/index/src/enime.adp.ts
+++ packages/index/src/enime.adp.ts
@@ -134,6 +134,6 @@ export const clicliAdapter = (item: any) => {
   }
 }
 
-const blackList = [3421, 31, 786, 3880, 2617]
+const blackList = [3421, 31, 786, 3880, 2617, 4087]
 
 export const cliclisAdapter = (data: any[] = []) => data.filter((it) => !blackList.includes(it.id)).map(clicliAdapter)
